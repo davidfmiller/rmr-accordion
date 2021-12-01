e82a3c8f750d5b809de280908136dc703446d34c
David Miller <davidfmiller@me.com>
2021-12-01 09:02:18 -0700

    docs

--- src/scripts/rmr-accordion.js
+++ src/scripts/rmr-accordion.js
@@ -6,50 +6,50 @@
   'use strict';
 
 
-  const enhanceMarkup = (accordion, index, config) => {
+  const enhance = (accordion, index, config) => {
 
-    const collapse = element => {
+    const collapse = node => {
       // get the height of the element's inner content, regardless of its actual size
-      const height = element.scrollHeight;
+      const height = node.scrollHeight;
 
       // temporarily disable all css transitions
-      const elementTransition = element.style.transition;
-      element.style.transition = '';
+      const elementTransition = node.style.transition;
+      node.style.transition = '';
 
       // on the next frame (as soon as the previous style change has taken effect),
       // explicitly set the element's height to its current pixel height, so we
       // aren't transitioning out of 'auto'
-      requestAnimationFrame(function() {
-          element.style.height = `${height}px`;
-          element.style.transition = elementTransition;
+      requestAnimationFrame(() => {
+        node.style.height = `${height}px`;
+        node.style.transition = elementTransition;
 
-          // on the next frame (as soon as the previous style change has taken effect),
-          // have the element transition to height: 0
-          requestAnimationFrame(function() {
-              element.setAttribute('aria-hidden', 'true')
-              element.style.height = null;
-          });
+        // on the next frame (as soon as the previous style change has taken effect),
+        // have the element transition to height: 0
+        requestAnimationFrame(function() {
+          node.setAttribute('aria-hidden', 'true')
+          node.style.height = null;
+        });
       });
     };
 
-    const expand = element => {
+    const expand = node => {
       // get the height of the element's inner content, regardless of its actual size
-      var height = element.scrollHeight;
+      var height = node.scrollHeight;
 
       // have the element transition to the height of its inner content
-      element.style.height = height + 'px';
+      node.style.height = height + 'px';
 
       const tidyUp = () => {
-          element.removeEventListener('transitionend', tidyUp);
-          // remove "height" from the element's inline styles, so it can return to its initial value
-          element.style.height = null;
+        node.removeEventListener('transitionend', tidyUp);
+        // remove "height" from the element's inline styles, so it can return to its initial value
+        node.style.height = null;
       };
 
       // when the next css transition finishes (which should be the one we just triggered)
-      element.addEventListener('transitionend', tidyUp);
+      node.addEventListener('transitionend', tidyUp);
     };
 
-    const accordionClick = e => {
+    const click = e => {
       e.preventDefault()
 
       const
@@ -80,7 +80,6 @@
       }
     };
 
-
     const
       id = `rmr-accordion-${index}`,
       isOpen = accordion.classList.contains('rmr-open'),
@@ -103,25 +102,25 @@
       button.setAttribute('aria-expanded', isOpen);
       button.setAttribute('aria-controls', id);
     }
-    title.addEventListener('click', accordionClick);
+    title.addEventListener('click', click);
     accordion.setAttribute('data-rmr-init', true);
   };
 
 
-  const initAccordions = (config) => {
+  const init = (config) => {
 
     let
-      count = 0,
-      accordion;
+      i = 0,
+      n;
 
-    while ((count === 0) || accordion) {
-      accordion = document.querySelector('.rmr-accordion:not([data-rmr-init]');
-      if (accordion) {
-        enhanceMarkup(accordion, count, config);
+    while (i === 0 || n) {
+      n = document.querySelector('.rmr-accordion:not([data-rmr-init]');
+      if (n) {
+        enhance(n, i, config);
       }
-      count++;
+      i++;
     }
   };
 
-  module.exports = initAccordions;
+  module.exports = init;
 })();
